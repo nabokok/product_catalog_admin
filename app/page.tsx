2bbf69d97fc7913d2c86f106fb7d5837d6bdd026
Alyona Nabok <alyonana42@gmail.com>
2024-03-19 22:37:05 +0200

    prisma setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import prisma from "@/lib/prisma";
 
+export const getProducts = async () => {
+  const products = await prisma.product.findMany();
+  return products;
+};
 
-export default function Home() {
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products)
   return (
     <main>
     Hello
