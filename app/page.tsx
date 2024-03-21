5bc86ccdb7734137e7adf0469cfd7ee74305d504
Nazarii <kostrubitskiiwork@gmail.com>
2024-03-22 00:27:12 +0200

    merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 
-export default function Page(): never {
-  redirect('/dashboard/products');
-}
-// import { getUserSession } from '@/lib/session'
-
-// export default async function Home() {
-//   const user = await getUserSession()
-//   return <main className="">{JSON.stringify(user)}</main>
+// export default function Page(): never {
+//   redirect('/dashboard/products');
 // }
+
+import { getUserSession } from '@/lib/session'
+
+export default async function Home() {
+  const user = await getUserSession()
+  return <main className="">{JSON.stringify(user)}</main>
+}
