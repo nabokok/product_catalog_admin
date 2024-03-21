c95cdd8d26e2e8b6b59267cfbbd5fe471998f1c9
Alyona Nabok <alyonana42@gmail.com>
2024-03-21 16:42:25 +0200

    add post method

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -1,6 +1,36 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(req: NextRequest, {params}: {params: {productId: string}}) {
+    try {
+        if (!params.productId) {
+            return new NextResponse("Product id is required", { status: 400 });
+        }
+
+
+        const existingProduct = await prisma.product.findUnique({
+            where: {
+                id: params.productId
+            }
+        });
+
+        if (!existingProduct) {
+            return new NextResponse("Product not found", { status: 404 });
+        }
+
+        const product = await prisma.product.findUnique({
+            where: {
+              id: params.productId
+            }
+          });
+
+          return NextResponse.json(product);
+
+    } catch(error) {
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
+
 export async function DELETE(req: NextRequest, {params}: {params: {productId: string}}) {
     try {
         if (!params.productId) {
@@ -22,9 +52,8 @@ export async function DELETE(req: NextRequest, {params}: {params: {productId: st
             },
         });
         
-        return new NextResponse(JSON.stringify(deletedProduct), { status: 200 });
+        return new NextResponse(null, { status: 200 });
     } catch (error) {
-        console.error("Error deleting product:", error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
