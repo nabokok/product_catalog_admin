9593b976676af797e235bfdbd1ae03153633b5fd
Alyona Nabok <alyonana42@gmail.com>
2024-03-22 16:24:10 +0200

    add auth via github

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+// Layout.tsx
+
 import * as React from 'react';
 import type { Viewport } from 'next';
 
@@ -6,22 +8,27 @@ import '@/styles/global.css';
 import { UserProvider } from '@/contexts/user-context';
 import { LocalizationProvider } from '@/components/core/localization-provider';
 import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
+import { Providers } from '@/components/Providers';
 
-export const viewport = { width: 'device-width', initialScale: 1 } satisfies Viewport;
+
+
+export const viewport = { width: 'device-width', initialScale: 1 } as Viewport;
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps): React.JSX.Element {
+export default function Layout({ children }: LayoutProps): React.ReactElement {
   return (
     <html lang="en">
       <body>
-        <LocalizationProvider>
-          <UserProvider>
-            <ThemeProvider>{children}</ThemeProvider>
-          </UserProvider>
-        </LocalizationProvider>
+        <Providers>
+          <LocalizationProvider>
+            <UserProvider>
+              <ThemeProvider>{children}</ThemeProvider>
+            </UserProvider>
+          </LocalizationProvider>
+        </Providers>
       </body>
     </html>
   );
