6c72b8ec238f8e2f9052c781ebce66e9a8a8691c
Vlad Pashko <vladpavloggg@gmail.com>
2024-03-23 20:43:59 +0200

    added create form

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 'use client'
 
+import { CreateProduct } from "@/components/dashboard/createProduct/createProduct"
 import { paths } from "@/constants/paths"
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
 
 export default function Home() {
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect(paths.auth.signIn)
-    },
-  })
-
-  if (session) {
-    redirect(paths.dashboard.products)
-  }
-
-  return null;
+  return (
+    <CreateProduct />
+  )
 }
