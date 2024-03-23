5d708078664ea9a80c5ddef17516ba1fbe53095f
Vlad Pashko <vladpavloggg@gmail.com>
2024-03-23 20:45:41 +0200

    fixed basic page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <CreateProduct />
-  )
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect(paths.auth.signIn)
+    },
+  })
+
+  if (session) {
+    redirect(paths.dashboard.products)
+  }
+
+  return null;
 }
