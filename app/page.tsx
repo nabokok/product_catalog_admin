73f9f7f9468bb4b3935b438da7ee24a7e5d2e98e
Yevhen <steplerjeka@gmail.com>
2024-03-23 11:05:43 +0200

    before merge

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,36 @@
 // export default function Page(): never {
 //   redirect('/dashboard/products');
 // }
+'use client';
+import { getUserSession } from '@/lib/session';
+import { Product } from '@prisma/client';
+import { useEffect, useState } from 'react';
+import ProductsTable from './components/ProductsTable';
 
-import { getUserSession } from '@/lib/session'
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default async function Home() {
-  const user = await getUserSession()
-  return <main className="">{JSON.stringify(user)}</main>
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('/api/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch products');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+  return (
+    <main className="">
+      <ProductsTable products={products} />
+    </main>
+  );
 }
