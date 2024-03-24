eb7b68fda340217bedfbc963e50bd4e1ff151507
Alyona Nabok <alyonana42@gmail.com>
2024-03-24 14:54:19 +0200

    add fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { CreateProduct } from "@/components/dashboard/createProduct/createProduct"
 import { paths } from "@/constants/paths"
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
